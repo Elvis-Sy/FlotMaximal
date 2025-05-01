edbdb2abd5163509127fee6e26cdd028431b260d
Elvis-Sy <elvissy04@gmail.com>
2025-05-01 17:34:52 +0300

    re-build

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,7 +6,7 @@ const LandingPage = () => {
   return (
     <div className="min-h-screen bg-[url('/assets/heros.jpg')] bg-cover bg-center">
       <Header/>
-      
+
       <div className="flex h-full flex-col gap-14">
         <div className="flex-col py-6 px-4 md:px-6">
           <h1 className='font-transcity text-[70px] md:text-[80px] mt-16 text-[#166cb7]'>
@@ -25,7 +25,7 @@ const LandingPage = () => {
             </p>
           </Link>
 
-          <Link to="/documentation" className="group py-6 px-4 shadow-lg rounded-lg max-w-80 bg-white/80 backdrop-blur-sm transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-xl hover:translate-y-2">
+          <Link to="/assisted" className="group py-6 px-4 shadow-lg rounded-lg max-w-80 bg-white/80 backdrop-blur-sm transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-xl hover:translate-y-2">
             <h1 className='font-bold text-lg text-[#127ad0]'>
               Mode Assisté
             </h1>
